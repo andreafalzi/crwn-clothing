b4d08d8da32f394c154bc8e8480e1f17ed7d2754
Andrea Falzi <af@andreafalzi.dev>
2022-06-08 14:35:08 +0200

    firebase auth

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,14 +1,40 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utilities/firebase/firebase.utilities';
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
+import { auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithFacebookPopup, signInWithGoogleRedirect } from '../../utilities/firebase/firebase.utilities';
 
 const SignIn = () => {
+  //implementing a redirect log-in, where we use the useEffect and getRedirectResult to track the response after the user has left our webapp to log-in and when it comes back our website is waiting for the information to get stored
+  useEffect(
+    () =>
+      async function logGoogleRedirectUser() {
+        const response = await getRedirectResult(auth);
+        console.log(response);
+        if (response) {
+          // eslint-disable-next-line no-unused-vars
+          const userDocRef = await createUserDocumentFromAuth(response.user);
+        }
+      },
+    []
+  );
+
   const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(response.user);
+    const { user } = await signInWithGooglePopup();
+    // eslint-disable-next-line no-unused-vars
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
+
+  const logFacebookUser = async () => {
+    const { user } = await signInWithFacebookPopup();
+    // eslint-disable-next-line no-unused-vars
+    const userDocRef = await createUserDocumentFromAuth(user);
   };
   return (
     <>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popus</button>
+      <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
+      <button onClick={logFacebookUser}>Sign in with Facebook Popus</button>
     </>
   );
 };
--- src/utilities/firebase/firebase.utilities.js
+++ src/utilities/firebase/firebase.utilities.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { async } from '@firebase/util';
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -17,16 +16,30 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+// eslint-disable-next-line no-unused-vars
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
   promt: 'select_account',
 });
 
+facebookProvider.setCustomParameters({
+  promt: 'select_account',
+});
+
+//auth is a firebase method that give us the chance to keep tracking the authentication of our user even if it leave our website
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+//Google SIGN-IN
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
+
+//Facebook SIGN-IN
+export const signInWithFacebookPopup = () => signInWithPopup(auth, facebookProvider);
+export const signInWitFacebookRedirect = () => signInWithRedirect(auth, facebookProvider);
 
 export const db = getFirestore();
 
@@ -34,17 +47,16 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   // doc() call from the database=db a collection='users' and looking for the document with the uniqueID=userAuth.uid
   const userDocRef = doc(db, 'users', userAuth.uid);
 
-  console.log(userDocRef);
-
   //getDoc retrive the document and we use the method .exists() to check if the document is already inside the collection 'users'
   const userSnapshot = await getDoc(userDocRef);
-  console.log(userSnapshot.exists());
+  // console.log(userSnapshot.exists());
 
   //logic to check if userSnapshot exists or not
   //if user data does not exist
   //create / set the document with the data from userAuth in my collection
   if (!userSnapshot.exists()) {
     const { displayName, email } = userAuth;
+    const providerData = userAuth.providerData[0].providerId;
     const createdAt = new Date();
 
     try {
@@ -53,6 +65,7 @@ export const createUserDocumentFromAuth = async (userAuth) => {
         displayName,
         email,
         createdAt,
+        providerData,
       });
     } catch (error) {
       console.log('error creating the user', error.message);
