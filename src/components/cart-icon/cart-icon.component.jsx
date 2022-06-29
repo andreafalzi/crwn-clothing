5ab708fafdd29364afc7855501a242960015a636
Andrea Falzi <andrea.falzi1988@gmail.com>
2022-06-29 15:28:07 +0200

    cart-item component + functionality cart-dropdown

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,13 +7,22 @@ import { CartContext } from '../../contexts/cart.context';
 import './cart-icon.style.scss';
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartItems, cartCount } = useContext(CartContext);
+  //Course Idea
+
+  //Personal Idea
+  // const itemsQuantity = cartItems.map((e) => {
+  //   return e.quantity;
+  // });
+
+  // const itemCounts = itemsQuantity.reduce((prevValue, currentValue) => prevValue + currentValue);
 
   const handleIsCartOpen = () => setIsCartOpen(!isCartOpen);
   return (
     <div className='cart-icon-container' onClick={handleIsCartOpen}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>0</span>
+      {/* <span className='item-count'>{itemCounts}</span> */}
+      <span className='item-count'>{cartCount}</span>
     </div>
   );
 };
